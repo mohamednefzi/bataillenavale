536b8c803ec56a9ab8a823815ff4b30cbcc70ac9
Ashijo <ligno19@gmail.com>
2017-09-15 09:41:34 -0400

    constructeur de flotte manuel

--- src/flotte/Flotte.java
+++ src/flotte/Flotte.java
@@ -2,26 +2,46 @@ package flotte;
 
 import java.util.ArrayList;
 
+import model.Pos;
+
 public class Flotte {
 
 	private ArrayList<Bateau> flotte;
+	private int tailleFlotte;
 	
+	////////-----------CONSTRUCTEURS DE FLOTTE IA------------------//////////////
 	
-	////////-----------CONSTRUCTEURS DE FLOTTE------------------//////////////
-	
-	public Flotte() {	
-		boolean[] newDirection= setRandDir(4);
+	public Flotte(int qty) {	
+		boolean[] newDirection= setRandDir(qty);
+		this.tailleFlotte = qty;
 		
-		for(int i=0; i<4; i++) {
+		for(int i=0; i<qty; i++) {
+			int newPosX;
+			int newPosY; 
+			
 			int j=i+1;
 			if(j == 1) {j=3;}
-			int newPosX = setRandPos(newDirection[i], j, true);
-			int newPosY = setRandPos(newDirection[i], j, false);
-			flotte.add(new Bateau(newPosX, newPosY, newDirection[i], 4 ));		
+			
+			boolean noColision;
+			do {
+				noColision = true;
+				newPosX = setRandPos(newDirection[i], j, true);
+				newPosY = setRandPos(newDirection[i], j, false);
+				
+				ArrayList<Pos> poss = new Bateau(newPosX, newPosY, newDirection[i], j ).getAllPos();
+				
+				for(int k=0; k<j; k++) {	
+					if(collision(poss.get(k))) {
+						noColision = false;
+					}
+				}
+				
+			}while (!noColision);
+			flotte.add(new Bateau(newPosX, newPosY, newDirection[i], j ));
+			
+			
 		}
 	}
-	
-	
 	private boolean[] setRandDir(int nbs) {
 		boolean[] dirArray = new boolean[nbs];
 		for(int i = 0; i<nbs; i++) {
@@ -29,8 +49,6 @@ public class Flotte {
 		}
 		return dirArray;
 	}
-	
-	
 	private int setRandPos( boolean dir, int size, boolean type) {
 		int newPos;
 		
@@ -43,17 +61,40 @@ public class Flotte {
 		else {
 			newPos = (int) ((Math.random()*10)%(10-size)); 
 		}
-		return newPos;
-		
-		
-		
-		
+		return newPos;	
 	};
+	
+	/////////--------------Constructeur de flotte Manuel-------------/////////////
+	public Flotte() {
+			
+	}
+	public void setNewShip() {
+		
+	}
+	
+	
+	
 	
 	
 	///////---------------------METHODES-----------------///////////////////
 	
-
+	public boolean collision(Pos aPos) {
+		boolean result = false;
+		
+		for(int i = 0; i<4; i++) {
+			ArrayList<Pos> poss = flotte.get(i).getAllPos();
+			
+			for (int j =0; j<flotte.get(i).getTaille();i++) {
+				
+				if(aPos.compareTo(poss.get(j))) {result = true;}
+				
+			}
+			
+		}
+		
+		
+		return result;
+	}
 	
 	
 	
